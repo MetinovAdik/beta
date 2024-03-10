fe7ad142a7a5bb6d5986663158a326103c08df54
MetinovAdik <metinovadilet@gmail.com>
2024-03-11 01:43:55 +0600

    +video processing

--- src/main/java/com/chatbot/tele/AudioMessageHandler.java
+++ src/main/java/com/chatbot/tele/AudioMessageHandler.java
@@ -135,11 +135,11 @@ public class AudioMessageHandler {
             // Распознавание речи с новым возвращаемым типом
             TranscriptionResult transcriptionResult = speechRecognitionService.recognizeSpeechFromAudioLocal(audioPath);
             if (!Files.exists(audioPath)) {
-                logger.error("Audio file does not exist after extraction: {}", audioPath);
+                logger.error("Audio file does not exist after TranscriptionResult transcriptionResult = speechRecognitionService.recognizeSpeechFromAudioLocal(audioPath);: {}", audioPath);
                 telegramService.sendMessage(chatId, "Произошла ошибка при обработке видео: аудиофайл не был создан.");
                 return; // Выход из метода, если аудиофайл не существует
             } else {
-                logger.info("Audio file exists and ready for further processing: {}", audioPath);
+                logger.info("Audio file exists and ready for further processing2: {}", audioPath);
             }
             if (transcriptionResult.getSegments().isEmpty()) {
                 telegramService.sendMessage(chatId, "Не удалось распознать текст.");
